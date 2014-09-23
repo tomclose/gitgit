59576ef369da3b92116fe24b7da3b693317dcca3
Tom Close <tom.close@cantab.net>
2014-09-23 16:05:59 -0700

    Experimenting with Thor and ruby-git

--- gitgit.gemspec
+++ gitgit.gemspec
@@ -18,6 +18,9 @@ Gem::Specification.new do |spec|
   spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
   spec.require_paths = ["lib"]
 
+  spec.add_runtime_dependency 'thor', '~> 0.19.1'
+  spec.add_runtime_dependency 'git', '~> 1.2.8'
+
   spec.add_development_dependency "bundler", "~> 1.6"
   spec.add_development_dependency "rake"
 end
--- lib/gitgit.rb
+++ lib/gitgit.rb
@@ -1,10 +1,32 @@
 require "gitgit/version"
+require "thor"
+require "git"
 
 module Gitgit
   # Your code goes here...
-  class Chatter
-    def say_hello
-      puts "Hello there!"
+  class CLI < Thor
+    include Thor::Actions
+
+    desc "init", "Initialise a git repo"
+    def init
+      Git.init
+      say "Git repo created! Let's git going ...", :green
     end
+
+    desc "save", "Save you changes to the repo"
+    def save
+      yes? "Do you really want to save?"
+      m = ask "Give a short description of the work you're saving: "
+      g = Git.open('.')
+      g.add(all:true)
+      g.commit(m)
+    end
+
+    desc "lg", "Show your recent saves"
+    def lg
+      g = Git.open('.')
+      g.log(20).each {|l| puts l }
+    end
+
   end
 end
